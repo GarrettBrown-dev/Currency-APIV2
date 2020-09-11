303a395999e8b9964d24b3f9636a9f514d303258
Garrett <gman9mm@live.com>
2020-09-11 13:54:17 -0700

    Begin getElements function.

--- src/main.js
+++ src/main.js
@@ -3,24 +3,33 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeService from './service/exchange-service.js';
+import { Currency } from '../service/exchange-service';
 
-function getElements(response) {
-  if (response.main) {
-    $(`.showConversion`).text(`The conversion for ${currency} is ${response.main.conversion_rates}`)
-  } else {
-    $('.showErrors').text(`There was an error: ${response}`);
-  }
-}
+let exchangeService = new ExchangeService
 
-async function makeApiCall(currency) {
-  const response = await ExchangeService.getRate(currency);
-  getElements(response);
-}
+let request = new XMLHttpRequest();
+
+const exchangeRateUrl = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`
 
 $(document).ready(function () {
   $('#conversionRate').click(function () {
+    event.preventDefault();
     let currency = $('#money').val();
-    clearFields();
-    makeApiCall(currency);
   });
-});
+
+  request.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      let currency = new Currency(response.base_code, response.conversion_rates);
+      getElements(currencyExchange);
+    }
+  }
+
+  request.open("GET", exchangeRateUrl, true);
+  request.send();
+
+  function getElements(currencyExchange) {
+    for (let i = 0; i < currencyExchange.length; i++)
+    
+
+})
