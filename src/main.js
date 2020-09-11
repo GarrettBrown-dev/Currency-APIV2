d57e14e676b91bc0bfb5365aadd03bd39045b4ee
Garrett <gman9mm@live.com>
2020-09-11 10:25:29 -0700

    Add async function for calling back to API, as well as doc-ready function outline.

--- src/main.js
+++ src/main.js
@@ -6,9 +6,22 @@ import exchangeService from './service/exchangeService.js';
 
 function getElements(response) {
   if (response.main) {
-    $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
-    $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    $(`.showConversion`).text(`The conversion for ${currency} is ${response.main.conversion_rates}`)
+    debugger
   } else {
     $('.showErrors').text(`There was an error: ${response}`);
   }
 }
+
+async function makeApiCall(currency) {
+  const response = await CurrencyService.getRate(currency);
+  getElements(response);
+}
+
+$(document).ready(function () {
+  $('#').click(function () {
+    let currency = $('#').val();
+    clearFields();
+    makeApiCall(currency);
+  });
+});
