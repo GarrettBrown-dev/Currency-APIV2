85e2b3b8d157e6549e04c9e56e363cc0b6441c07
Garrett <gman9mm@live.com>
2020-09-11 09:53:24 -0700

    Add export default for currency.

---
+++ service/exchangeService.js
@@ -0,0 +1,13 @@
+export default class CurrencyService {
+  static async getRate(currency) {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch (error) {
+      return error.message;
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -3,3 +3,12 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import exchangeService from './service/exchangeService.js';
+
+function getElements(response) {
+  if (response.main) {
+    $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
+    $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+  } else {
+    $('.showErrors').text(`There was an error: ${response}`);
+  }
+}
