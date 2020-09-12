e8d2d96db8b07f373bc9c2bdc54a80751a7cd7ce
Garrett <gman9mm@live.com>
2020-09-11 17:45:04 -0700

    Resolved submit button issues, got called API info into console. Done for today.

--- src/main.js
+++ src/main.js
@@ -1,43 +1,41 @@
 import $ from 'jquery';
 // import CurrencyService, { Currency } from './service/currency-services';
 
-// let currencyService = new CurrencyService
 
-// let request = new XMLHttpRequest();
-
-
-
-$(document).ready(function () {
-  $('#submit-add').submit(function (event) {
+$(document).ready(() => {
+  $('#submit-add').click(async function (event) {
     event.preventDefault();
-    console.log("currency");
-    let currency = $('select#base_code').val();
-    console.log(currency);
+    const currency = $('select#currencies').val();
     const exchangeRateUrl = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`;
-    fetch(exchangeRateUrl).then(function (response) {
-      console.log(response);
 
-    });
+    try {
+      const response = await fetch(exchangeRateUrl);
+      const jsonResponse = await response.json();
+    } catch (error) {
+      console.log("error: ", error);
+      // alert("sup wit this error");
+    }
 
-
-
-    // request.onreadystatechange = function (response) {
-    //   if (this.readyState === 4 && this.status === 200) {
-    //     const response = JSON.parse(this.response);
-    //     let currency = new Currency(response.base_code, response.conversion_rates);
-    //     currencyService.addCurrency(currency)
-    //     getElements(currencyService);
-    //   }
-    // }
-
-    // request.open("GET", exchangeRateUrl, true);
-    // request.send();
-
-    // function getElements(currencyService) {
-    //   for (let i = 0; i < currencyService.length; i++) {
-    //     $(`#result`).html(` <div id="$(currencyService[i].conversion_rates"></div>`)
-    //     $("$(currencyService[i].conversion_rates").text$(currencyService[i].conversion_rates)
-    //   }
-    // }
   });
 });
+
+
+
+// request.onreadystatechange = function (response) {
+//   if (this.readyState === 4 && this.status === 200) {
+//     const response = JSON.parse(this.response);
+//     let currency = new Currency(response.base_code, response.conversion_rates);
+//     currencyService.addCurrency(currency)
+//     getElements(currencyService);
+//   }
+// }
+
+// request.open("GET", exchangeRateUrl, true);
+// request.send();
+
+// function getElements(currencyService) {
+//   for (let i = 0; i < currencyService.length; i++) {
+//     $(`#result`).html(` <div id="$(currencyService[i].conversion_rates"></div>`)
+//     $("$(currencyService[i].conversion_rates").text$(currencyService[i].conversion_rates)
+//   }
+// }
