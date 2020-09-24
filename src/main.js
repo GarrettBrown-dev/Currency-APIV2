93e36fe1f6a5e725cdd68b3a4eee322f5c5d138c
Garrett <gman9mm@live.com>
2020-09-23 17:00:35 -0700

    Reaarange and refactor code to properly match with expectations. Special thanks to Travis for the help!

--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
 import $ from 'jquery';
-import CurrencyService, { Currency } from './service/currency-services';
+import CurrencyService from './service/currency-service';
 
 $(document).ready(function () {
+  const select = document.querySelectorAll('select');
+  const input = document.querySelectorAll('input');
+  let html = '';
+
   async function currency() {
-    const res = await fetch(API_URL);
-    const data = await res.json();
-    const arrKeys = Object.keys(data.rates)
-    const rates = data.rates;
+    const data = await CurrencyService.Currency();
+    const arrKeys = Object.keys(data.conversion_rates);
+    const rates = data.conversion_rates;
 
     arrKeys.map(item => {
       return html += `<option value=${item}>${item}</option>`;
-    });
+    })
 
     for (let i = 0; i < select.length; i++) {
       select[i].innerHTML = html;
@@ -20,13 +23,16 @@ $(document).ready(function () {
       input[i].value = input[j].value * rates[select[i].value] / rates[select[j].value];
     }
 
-    input[0].addEventListener('keyup', () => (1, 0))
+    input[0].addEventListener('keyup', () => convert(1, 0));
 
-    input[1].addEventListener('keyup', () => (0, 1))
+    input[1].addEventListener('keyup', () => convert(0, 1));
 
-    select[0].addEventListener('change', () => (1, 0));
+    select[0].addEventListener('change', () => convert(1, 0));
 
-    select[1].addEventListener('change', () => convert(0, 1));
+    select[0].addEventListener('change', () => convert(0, 1));
+
 
   }
+
+  currency();
 })
--- src/service/currency-service.js
+++ src/service/currency-service.js
@@ -1,8 +1,10 @@
-// export default class CurrencyService {
-// export default class CurrencyService {
-//   class CurrencyService {
-//   const select = document.querySelectorAll('select');
-//   const input = document.querySelectorAll('input');
-//   const API_URL = "`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`;";
-//     let html = '';
-//   }
+export default class CurrencyService {
+
+  static async Currency() {
+    const API_URL = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+    // `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest`;
+    const res = await fetch(API_URL);
+    const data = await res.json();
+    return data
+  }
+}
